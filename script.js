8f90de1f071608f5f7aef55578a9ad15c6c0e8ff
DenisMatysik <denismatysik@gmail.com>
2023-05-16 15:32:46 +0300

    add styles for width 1920px

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("script work");
+//Для элементов 2-ой секции
+const navItems = document.querySelectorAll(".main-section2-projects-nav span");
+navItems.forEach(item => {
+	item.addEventListener("click", () => {
+		// Удаление активного класса у всех элементов
+		navItems.forEach(navItem => navItem.classList.remove("active"));
+		// Добавление активного класса на текущий элемент
+		item.classList.add("active");
+	});
+});
