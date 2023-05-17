f6087e2a39317caf426b32accc38284453cab249
DenisMatysik <denismatysik@gmail.com>
2023-05-17 23:25:40 +0300

    fix code view

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ navItems.forEach(item => {
 
 //Прячем все остальные article, когда открываем игру
 const articlesArr = document.querySelectorAll("main article");
-const closeOtherArticles = () => {
+const closeOtherArticles = (articleName = "gamePage") => {
 	articlesArr.forEach(article => {
-		if (article.getAttribute("id") !== "gamePage") {
+		if (article.getAttribute("id") !== articleName) {
 			article.style.display = "none";
 		} else {
 			article.style.display = "block";
@@ -34,31 +34,3 @@ arrGames.forEach(el => {
 		}
 	});
 });
-
-//gamePage - карусель обоев
-let mainImage = document.querySelector(".gamePage-section2-gameInf-mainImg img");
-let slides = document.getElementsByClassName("slide");
-const prevButton = document.querySelector(".prev-button");
-const nextButton = document.querySelector(".next-button");
-let currentIndex = 0;
-
-showSlide(currentIndex);
-
-prevButton.addEventListener("click", function () {
-	showSlide(currentIndex - 1);
-});
-
-nextButton.addEventListener("click", function () {
-	showSlide(currentIndex + 1);
-});
-
-function showSlide(index) {
-	// Проверить границы индекса
-	if (index < 0) {
-		index = slides.length - 1;
-	} else if (index >= slides.length) {
-		index = 0;
-	}
-	currentIndex = index;
-	mainImage.setAttribute("src", slides[index].children[0].getAttribute("src")); // Показать текущий слайд
-}
