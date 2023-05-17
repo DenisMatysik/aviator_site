d93ba22c7249ebf6e850a542f8ced4199181d747
DenisMatysik <denismatysik@gmail.com>
2023-05-17 15:10:15 +0300

    add gamePage

--- script.js
+++ script.js
@@ -8,3 +8,33 @@ navItems.forEach(item => {
 		item.classList.add("active");
 	});
 });
+
+//для карусели обоев
+let mainImage = document.querySelector(".gamePage-section2-gameInf-mainImg img");
+let slides = document.getElementsByClassName("slide");
+const prevButton = document.querySelector(".prev-button");
+const nextButton = document.querySelector(".next-button");
+let currentIndex = 0;
+
+showSlide(currentIndex);
+
+prevButton.addEventListener("click", function () {
+	showSlide(currentIndex - 1);
+});
+
+nextButton.addEventListener("click", function () {
+	showSlide(currentIndex + 1);
+});
+
+function showSlide(index) {
+	// Проверить границы индекса
+	if (index < 0) {
+		index = slides.length - 1;
+	} else if (index >= slides.length) {
+		index = 0;
+	}
+	currentIndex = index;
+	if (currentIndex != 0) {
+		mainImage.setAttribute("src", slides[index].children[0].getAttribute("src")); // Показать текущий слайд
+	}
+}
