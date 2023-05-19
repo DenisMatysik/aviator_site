021cc598a5dea5b3458beb47f05caa745b462965
DenisMatysik <denismatysik@gmail.com>
2023-05-19 10:51:31 +0300

    fix style

--- animationLine.js
+++ animationLine.js
@@ -1,16 +1,20 @@
 const element = document.querySelector(".blue-point");
 const container = document.querySelector(".mainPage-section1-image-block");
 let width = 0;
-
+let startX = 0;
 export const animateLine = () => {
 	width += 0.5; // Увеличение ширины линии
-	element.style.width = width + "px"; // Изменение ширины элемента
-	if (width < container.offsetWidth) {
+	startX += 0.5; // Увеличение позиции элемента по горизонтали
+	element.style.width = width + "px"; // Изменениеирины элемента
+	element.style.left = startX + "px"; // Изменение позиции элемента по горизонтали
+	if (width <= container.offsetWidth) {
 		requestAnimationFrame(animateLine); // Продолжаем анимацию
 	}
 };
 
 export const resetLine = () => {
 	width = 0;
+	startX = 0;
+	element.style.left = 0 + "px"; // Изменение ширины элемента
 	element.style.width = 0 + "px"; // Изменение ширины элемента
 };
